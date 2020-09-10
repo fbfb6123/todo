fe54ec12e4a526fa10766285be87ed6f9457ac78
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-10 22:51:26 +0900

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Tasks;
 use Illuminate\Http\Request;
+
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
         $folders = Folder::all();
+
+        $current_folder = Folder::find($id);
 
         return view('tasks/index', [
             'folders' => $folders,
