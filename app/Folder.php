7c84675efc0ae062e204c699a5ca4bab6d8a4a43
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-10 23:56:21 +0900

    Update Folder.php

--- app/Folder.php
+++ app/Folder.php
@@ -9,5 +9,6 @@ class Folder extends Model
     public function tasks()
     {
       return $this->hasMany('App\Task');
+      //省略せずに書いた場合、意味合いは一緒。$this->hasMany('App\Task', 'folder_id', 'id');
     }
 }
