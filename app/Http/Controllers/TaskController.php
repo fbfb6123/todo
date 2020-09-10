f0936c59e9a431ef96df011b8f8fb9ee607ba0d8
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-10 22:53:32 +0900

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,9 +11,14 @@ class TaskController extends Controller
 {
     public function index(int $id)
     {
+        //全てのフォルダを取得
         $folders = Folder::all();
 
+        //選択されたフォルダを取得
         $current_folder = Folder::find($id);
+
+        //選択されたタスクを取得する
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
 
         return view('tasks/index', [
             'folders' => $folders,
