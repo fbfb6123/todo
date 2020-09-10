6461cc9e5efc30fed04e6bf328d85723e0bf2daa
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-10 22:32:05 +0900

    Update 2020_09_10_132425_create_tasks_table.php

--- database/migrations/2020_09_10_132425_create_tasks_table.php
+++ database/migrations/2020_09_10_132425_create_tasks_table.php
@@ -15,7 +15,14 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('folder_id')->unsigned();
+            $table->string('title', 100);
+            $table->date('due_date');
+            $table->integer('status')->default(1);
             $table->timestamps();
+
+            //外部キー設定
+            $table->foreign('folder_id')->references('id')->on('folders');
         });
     }
 
