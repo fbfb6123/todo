84f547c582a9b6893829325534d47f25185dddfb
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-10 23:13:54 +0900

    Update Task.php

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    
+
+   /**
+    * 状態定義
+    */
+    const STATUS = [
+      1 => ['label' => '未着手'],
+      2 => ['label' => '着手中'],
+      3 => ['label' => '完了'],
+    ]
+   /**
+    * 状態のラベル
+    */
+
 }
