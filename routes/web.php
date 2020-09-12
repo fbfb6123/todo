f40853abcc99354c4c080ac7d33ce0c2fe93ecff
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-12 23:06:50 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@shoeCreateForm')->name('folders.create');
-Route::post('/folders/create' 'FolderController@create')
+Route::post('/folders/create' 'FolderController@create');
