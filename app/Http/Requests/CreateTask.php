6757e1c1f31452513bf403accdfc1f821adf62a3
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-13 17:14:01 +0900

    Update CreateTask.php

--- app/Http/Requests/CreateTask.php
+++ app/Http/Requests/CreateTask.php
@@ -24,10 +24,23 @@ class CreateTask extends FormRequest
     public function rules()
     {
         return [
+            'title' => 'required|max:100',
+            'due_date' => 'required|date|after_or_equal:today',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
             'title' => 'タイトル',
             'due_date' => '期限日',
         ];
     }
 
-    
+    public function messages()
+    {
+        return [
+            'due_date.after_or_equal' => ':attribute には今日以降の日付を入力してください。',
+        ];
+    }
 }
