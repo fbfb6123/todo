897ab5fd6935a8901a0879782e0fb9b3f22b7201
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-09-15 21:55:44 +0900

    Update 2020_09_15_125144_add_user_id_to_folders.php

--- database/migrations/2020_09_15_125144_add_user_id_to_folders.php
+++ database/migrations/2020_09_15_125144_add_user_id_to_folders.php
@@ -29,7 +29,7 @@ class AddUserIdToFolders extends Migration
     public function down()
     {
         Schema::table('folders', function (Blueprint $table) {
-            //
+            $table->dropColum('use_id');
         });
     }
 }
